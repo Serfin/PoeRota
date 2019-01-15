cdcd6cd8a66cf1653363c0400185cef022831c72
Gracjan Bryłka <gracjanbrylka@gmail.com>
2019-01-15 23:57:44 +0100

    Added possibility to add new user via request

--- PoeRota.Api/Controllers/UsersController.cs
+++ PoeRota.Api/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PoeRota.Infrastructure.Commands.Users;
 using PoeRota.Infrastructure.DTO;
 using PoeRota.Infrastructure.Services;
 
@@ -25,10 +26,13 @@ namespace PoeRota.Api.Controllers
             => await _userService.GetAllAsync();
 
         // GET users/id
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
+        [HttpGet("{email}")]
+        public async Task<UserDto> Get(string email)
+            => await _userService.GetAsync(email);
+
+        [HttpPost]
+        public async Task Register([FromBody] CreateUser request)
+            => await _userService.RegisterAsync(request.Username, 
+            request.Password, request.Email, request.Ign);
     }
 }
